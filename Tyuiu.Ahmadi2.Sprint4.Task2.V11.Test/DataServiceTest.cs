9624c1864dcc48c451a4e6c367c6be30fee7f28a
Ahmadi1 <Ahmadi@DS10BK3>
2025-11-18 05:15:03 -0800

    Task2

---
+++ Tyuiu.Ahmadi2.Sprint4.Task2.V11.Test/DataServiceTest.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Tyuiu.Ahmadi2.Sprint4.Task2.V11.Lib;
+
+namespace Tyuiu.Ahmadi2.Sprint4.Task2.V11.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCalculate()
+        {
+            DataService ds = new DataService();
+            int[] array = { 4, 5, 3, 6, 8, 7, 4, 5, 6, 8, 3, 4, 7, 8, 6 };
+            int res = ds.Calculate(array);
+
+            // اعداد زوج در آرایه: 4, 6, 8, 4, 6, 8, 4, 8, 6
+            int wait = 4 + 6 + 8 + 4 + 6 + 8 + 4 + 8 + 6;
+            Assert.AreEqual(wait, res);
+        }
+
+        [TestMethod]
+        public void ValidCalculateWithOnlyEven()
+        {
+            DataService ds = new DataService();
+            int[] array = { 4, 6, 8, 4, 6, 8, 4, 6, 8, 4, 6, 8, 4, 6, 8 };
+            int res = ds.Calculate(array);
+            int wait = 4 + 6 + 8 + 4 + 6 + 8 + 4 + 6 + 8 + 4 + 6 + 8 + 4 + 6 + 8;
+            Assert.AreEqual(wait, res);
+        }
+
+        [TestMethod]
+        public void ValidCalculateWithOnlyOdd()
+        {
+            DataService ds = new DataService();
+            int[] array = { 3, 5, 7, 3, 5, 7, 3, 5, 7, 3, 5, 7, 3, 5, 7 };
+            int res = ds.Calculate(array);
+            int wait = 0; 
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.Ahmadi2.Sprint4.Task2.V11/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Tyuiu.Ahmadi2.Sprint4.Task2.V11.Lib;
+
+namespace Tyuiu.Ahmadi2.Sprint4.Task2.V11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Random rand = new Random();
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            int[] array = new int[15];
+
+           
+            Console.Write("Массив: { ");
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = rand.Next(3, 9); 
+                Console.Write(array[i]);
+                if (i < array.Length - 1)
+                    Console.Write(", ");
+            }
+            Console.WriteLine(" }");
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            int result = ds.Calculate(array);
+            Console.WriteLine("Сумма чётных элементов массива = " + result);
+            Console.ReadKey();
+        }
+    }
+}
