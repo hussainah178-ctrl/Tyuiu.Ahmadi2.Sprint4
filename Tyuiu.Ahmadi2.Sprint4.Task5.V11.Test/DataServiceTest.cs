d7038b0456a396e5bb72ba7c859f054aa0cd7919
Ahmadi1 <Ahmadi@DS10BK3>
2025-11-19 05:28:05 -0800

    Task5

---
+++ Tyuiu.Ahmadi2.Sprint4.Task5.V11.Test/DataServiceTest.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Tyuiu.Ahmadi2.Sprint4.Task5.V11.Lib;
+
+namespace Tyuiu.Ahmadi2.Sprint4.Task5.V11.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCalculate()
+        {
+            DataService ds = new DataService();
+
+            int[,] matrix = new int[5, 5] {
+                {-5, -3, 2, -2, 1},
+                {-6, 1, -5, 0, 2},
+                {-1, 2, -4, 1, -4},
+                {4, -1, -4, -4, -5},
+                {-4, 6, -1, -1, 2}
+            };
+
+            int[,] res = ds.Calculate(matrix);
+            int[,] wait = new int[5, 5] {
+                {-5, -3, 1, -2, 1},
+                {-6, 1, -5, 0, 1},
+                {-1, 1, -4, 1, -4},
+                {1, -1, -4, -4, -5},
+                {-4, 1, -1, -1, 1}
+            };
+
+            CollectionAssert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.Ahmadi2.Sprint4.Task5.V11/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using Tyuiu.Ahmadi2.Sprint4.Task5.V11.Lib;
+
+namespace Tyuiu.Ahmadi2.Sprint4.Task5.V11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Random rand = new Random();
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            int[,] matrix = new int[5, 5];
+
+            Console.WriteLine("Массив 5x5 (случайные значения от -7 до 2):");
+            Console.WriteLine();
+
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    matrix[i, j] = rand.Next(-7, 3); // от -7 до 2 включительно
+                }
+            }
+
+            // Вывод исходного массива
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    Console.Write($"{matrix[i, j]} \t");
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                             *");
+            Console.WriteLine("***************************************************************************");
+
+            int[,] res = ds.Calculate(matrix);
+
+            Console.WriteLine("Измененный массив (положительные элементы заменены на 1):");
+            Console.WriteLine();
+
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    Console.Write($"{res[i, j]} \t");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+        }
+    }
+}
