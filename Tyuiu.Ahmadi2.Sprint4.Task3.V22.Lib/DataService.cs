7d32b77c7fd4112ba96728129fbc87f6618bf33e
Ahmadi1 <Ahmadi@DS10BK3>
2025-11-18 23:40:00 -0800

    Task3

---
+++ Tyuiu.Ahmadi2.Sprint4.Task3.V22.Lib/DataService.cs
@@ -0,0 +1,24 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint4;
+
+namespace Tyuiu.Ahmadi2.Sprint4.Task3.V22.Lib
+{
+    public class DataService : ISprint4Task3V22
+    {
+        public int Calculate(int[,] array)
+        {
+            int rows = array.GetLength(0);
+            int columns = array.GetLength(1);
+
+            int max = array[0, 0];
+
+            for (int j = 0; j < columns; j++)
+            {
+                if (array[0, j] > max)
+                {
+                    max = array[0, j];
+                }
+            }
+            return max;
+        }
+    }
+}
---
+++ Tyuiu.Ahmadi2.Sprint4.Task3.V22/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Tyuiu.Ahmadi2.Sprint4.Task3.V22.Lib;
+
+namespace Tyuiu.Ahmadi2.Sprint4.Task3.V22
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            int[,] array = new int[5, 5] {
+                {4, 4, 7, 8, 9},
+                {9, 5, 9, 7, 8},
+                {7, 4, 9, 4, 6},
+                {4, 4, 7, 4, 4},
+                {4, 5, 8, 6, 7}
+            };
+
+            Console.WriteLine("Массив:");
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    Console.Write(array[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Максимальный элемент в первой строке = " + ds.Calculate(array));
+            Console.ReadKey();
+        }
+    }
+}
